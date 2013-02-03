b26b8f418dd7531383e8ac5737c42f1b676ed9f6
Kernel-Saram <happydh1@naver.com>
2013-02-03 06:56:02 -0800

    clean sources

--- obj/KERNEL_OBJ/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Sat Jan 26 20:23:37 PST 2013"
-#define LINUX_COMPILE_BY "saram"
-#define LINUX_COMPILE_HOST "ubuntu"
-#define LINUX_COMPILER "gcc version 4.7.3 20121205 (prerelease) (Linaro GCC 4.7-2012.12) "
--- obj/KERNEL_OBJ/include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "3.0.8-CLUSTER-Kernel-g8c787fb-dirty"
